bae0a46b00d926411a5bd0e343a05ca30a733b86
tracyneproski <tracyneproski@gmail.com>
2022-10-22 21:25:49 -0500

    Add winning combos and round mechanic

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -15,6 +15,19 @@ class TicTacToe
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    
+    @moves_1 = []
+    @moves_2 = []
+
+    @winning_combos = 
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [7,5,3],]
   end
 
   def start
@@ -22,7 +35,56 @@ class TicTacToe
     puts "New game started! #{@player_1} vs. #{@player_2}"
     #puts "To exit game at any point, type 'exit'"
     
+  end
 
+  def exit move
+    if move == "exit"
+      puts "Thank you for playing!"
+    end
+  end
+
+  def occupied move
+    if moves_1.include?(move.to_i) || moves_2.include(move.to_i)
+      puts "Place is already occupied. Choose a different place:"
+    end
+  end
+
+  def check_for_win moves
+    @winning_combos.each do |combo|
+      if (combo - moves).empty?
+        puts "Congratulations! You win!"
+      end
+    end
+  end
+
+
+  def round
+    if @moves_1.size = @moves_2.size 
+      puts "#{@player_1} choose where to place an X:\n"
+
+      move_x = gets.chomp
+
+      exit move_x
+      occupied move_x
+
+      @moves_1.push(move_x.to_i).sort!
+      check_for_win @moves_1
+
+      round
+
+    else
+      puts "#{@player_2} choose where to place an O:\n"
+
+      move_o = gets.chomp
+
+      exit move_o
+      occupied move_o
+
+      @moves_2.push(move_o.to_i).sort!
+      check_for_win @moves_2
+
+      round
+    end
     
   end
 
